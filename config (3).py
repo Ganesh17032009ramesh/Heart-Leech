5559e4cf94bb0234fb0363a76c92db41f447f8f0
Ganesh17032009ramesh <ganeshramesh173@gmail.com>
2025-06-03 19:01:35 +0530

    Add files via upload

---
+++ config (3).py
@@ -0,0 +1,111 @@
+# REQUIRED CONFIG
+BOT_TOKEN = "8182148255:AAE8YjGvSuYylGnb8nbEcQcxmVaECxOLPRQ"
+OWNER_ID = "6149629813"
+TELEGRAM_API = "20902603"
+TELEGRAM_HASH = "79e5caa103a9e9fb0183390b4800845d"
+
+# SEMI-REQUIRED, WE SUGGEST TO FILL IT FROM MONGODB
+DATABASE_URL = "mongodb+srv://NarutoScrapper:17032009@cluster0.kekh4u4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# OPTIONAL CONFIG
+TG_PROXY = {}
+USER_SESSION_STRING = "BQE-8ssAghGu5b5zinp6g6fkd3LgkbCG7kYcKz-vxfxInNTPymYZAaIfjECSrR8qg2oc3dqSpWeHn2DcYTLNCenJXNutfsMKypuiGwr6FUQa08U2LOVxTgEfHlET-pIfOjg1zt9dDAn-weZ5h_4m-lTq4kZ8r98MW-DjyIFs_6m7uyjsLZ7PjWhxqEr5hLDSGWeD0PtovdTkC-03r9bP9dnoVR6BLIpgNa_NjFXcfHTNYEFrmASFO9D_Wu7SNDO3VjuuPdxExtUVtzr-Ai7LzYl2gFtQVgN7CRe7KT7m2ZlR5Uz52ZlYePG0gkD77H_3aIszRuVPNffkVP2aU6pOT5M207jWTQAAAAHnsayfAQ"
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
+CMD_SUFFIX = ""
+AUTHORIZED_CHATS = "6149629813"
+SUDO_USERS = ""
+DEFAULT_UPLOAD = "rc"
+FILELION_API = ""
+STREAMWISH_API = ""
+EXCLUDED_EXTENSIONS = ""
+INCOMPLETE_TASK_NOTIFIER = False
+YT_DLP_OPTIONS = ""
+USE_SERVICE_ACCOUNTS = False
+NAME_SUBSTITUTE = ""
+FFMPEG_CMDS = {}
+UPLOAD_PATHS = {}
+
+# INKYPINKY
+DELETE_LINKS = False
+FSUB_IDS = "-1002698901398"
+TOKEN_TIMEOUT = 0
+PAID_CHANNEL_ID = 0
+PAID_CHANNEL_LINK = ""
+SET_COMMANDS = True
+METADATA_KEY = ""
+WATERMARK_KEY = ""
+LOG_CHAT_ID = 0
+LEECH_FILENAME_CAPTION = ""
+HYDRA_IP = ""
+HYDRA_API_KEY = ""
+INSTADL_API = ""
+
+# GDrive Tools
+GDRIVE_ID = "1eEVaksmaUzFIvJI-UXZduDhHMvA4Q-S_"
+IS_TEAM_DRIVE = False
+STOP_DUPLICATE = False
+INDEX_URL = ""
+
+# Rclone
+RCLONE_PATH = ""
+RCLONE_FLAGS = ""
+RCLONE_SERVE_URL = ""
+RCLONE_SERVE_PORT = 0
+RCLONE_SERVE_USER = ""
+RCLONE_SERVE_PASS = ""
+
+# Mega credentials
+MEGA_EMAIL = ""
+MEGA_PASSWORD = ""
+
+# Sabnzbd
+USENET_SERVERS = [
+    {
+        "name": "main",
+        "host": "",
+        "port": 563,
+        "timeout": 60,
+        "username": "",
+        "password": "",
+        "connections": 8,
+        "ssl": 1,
+        "ssl_verify": 2,
+        "ssl_ciphers": "",
+        "enable": 1,
+        "required": 0,
+        "optional": 0,
+        "retention": 0,
+        "send_group": 0,
+        "priority": 0,
+    },
+]
+
+# Update
+UPSTREAM_REPO = "https://github.com/AeonOrg/Aeon-MLTB"
+UPSTREAM_BRANCH = "main"
+
+# Leech
+LEECH_SPLIT_SIZE = 0
+AS_DOCUMENT = False
+MEDIA_GROUP = False
+USER_TRANSMISSION = False
+HYBRID_LEECH = False
+LEECH_FILENAME_PREFIX = ""
+LEECH_DUMP_CHAT = ""
+THUMBNAIL_LAYOUT = ""
+
+# qBittorrent/Aria2c
+TORRENT_TIMEOUT = 0
+BASE_URL = ""
+BASE_URL_PORT = 80
+WEB_PINCODE = False
+
+# Queueing system
+QUEUE_ALL = 0
+QUEUE_DOWNLOAD = 0
+QUEUE_UPLOAD = 0
+
+# RSS
+RSS_DELAY = 600
+RSS_CHAT = "-1002698901398"
+RSS_SIZE_LIMIT = 0
